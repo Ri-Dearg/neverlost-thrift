79774b520f1923c6068c891c9c461362a0d509ee
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-05 20:31:16 +0000

    Updated test_views

--- products/test_views.py
+++ products/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Product
 
 
 class TestViews(TestCase):
@@ -6,4 +7,6 @@ class TestViews(TestCase):
     def test_render_index(self):
         page = self.client.get('/')
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed('index.html')
+        self.assertTemplateUsed('product_list.html')
+        self.assertQuerysetEqual(page.context['products'],
+                                 Product.objects.all().order_by('-date_added'))
