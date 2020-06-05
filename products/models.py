82b82c5dd4c9d0f9d9b2e17c2500996344153b74
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-05 19:18:17 +0000

    Created Products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.postgres import fields
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='images')
+    date_added = models.DateTimeField(default=timezone.now)
+    admin_tags = fields.ArrayField(models.CharField(max_length=40), size=7)
+    user_tags = fields.ArrayField(models.CharField(max_length=40),
+                                  size=7,
+                                  default=list)
+
+    class Meta:
+        ordering = ['-date_added']
+
+    def __str__(self):
+        return f'{self.name}, {self.price}'
