117192813cee8dba640f1685a7476419e78fd847
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-05 19:54:23 +0000

    Added image resizing feature for product model

--- products/models.py
+++ products/models.py
@@ -1,21 +1,54 @@
 from django.db import models
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 from django.utils import timezone
 from django.contrib.postgres import fields
 
 
 class Product(models.Model):
+    """Defines the Product Class."""
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(default='default.png',
+                              upload_to='product_images')
     date_added = models.DateTimeField(default=timezone.now)
     admin_tags = fields.ArrayField(models.CharField(max_length=40), size=7)
     user_tags = fields.ArrayField(models.CharField(max_length=40),
                                   size=7,
-                                  default=list)
+                                  default=list, blank=True)
+
+    def save(self, *args, **kwargs):
+        """Image resizing, snippet taken from:
+        https://djangosnippets.org/snippets/10597/ """
+        # Opening the image
+        img = Image.open(self.image)
+
+        # If the image is tto big, it is resized
+        if img.height > 500 or img.width > 500:
+
+            output_size = (500, 500)
+            img.thumbnail(output_size)
+
+            # Converts format while in memory
+            output = BytesIO()
+            img.save(output)
+            output.seek(0)
+
+            # Replaces the Imagefield value with the newly converted image
+            self.image = InMemoryUploadedFile(
+                output,
+                'ImageField',
+                f'{self.image.name}.{img.format}',
+                'image/jpeg', sys.getsizeof(output),
+                None)
+
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['-date_added']
 
     def __str__(self):
-        return f'{self.name}, {self.price}'
+        return f'{self.name}: €{self.price}'
