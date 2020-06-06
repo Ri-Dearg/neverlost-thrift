c0de8527bdeefa9aac10e6ddb9ac6330cfe2cafd
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-06 18:58:21 +0000

    Updated testing for ProductDetailView

--- products/test_views.py
+++ products/test_views.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 from .models import Product
 
+valid_product = Product(name='A product',
+                        blurb='string',
+                        description='second string',
+                        admin_tags=['this', 'is', 'an', 'array'],
+                        price=10.99)
+
 
 class TestViews(TestCase):
 
@@ -10,3 +16,16 @@ class TestViews(TestCase):
         self.assertTemplateUsed('product_list.html')
         self.assertQuerysetEqual(page.context['products'],
                                  Product.objects.all().order_by('-date_added'))
+
+    def test_render_product_detail(self):
+        valid_product.save()
+        page = self.client.get(f'/product/{valid_product.id}')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed('product_detail.html')
+        self.assertTrue(page.context['product'])
+
+    def test_render_404_not_found(self):
+        valid_product.save()
+        page = self.client.get('/product/0')
+        self.assertEqual(page.status_code, 404)
+        self.assertTemplateUsed('404.html')
