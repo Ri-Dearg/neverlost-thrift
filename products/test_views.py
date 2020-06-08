f317b567b8b1951e9157a82b86c6d6e4b3414c77
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-08 01:41:48 +0000

    Adjusted product image save, refactored tests, fixed bug in row display.

--- products/test_views.py
+++ products/test_views.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
-from .models import Product
+from .models import Product, Category
 
+category = Category(name='clothing')
+category.save()
 valid_product = Product(name='A product',
+                        category=category,
                         description='second string',
                         admin_tags=['this', 'is', 'an', 'array'],
                         price=10.99)
@@ -9,16 +12,18 @@ valid_product = Product(name='A product',
 
 class TestViews(TestCase):
 
+    valid_product.save()
+
     def test_render_index(self):
         page = self.client.get('/')
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed('product_list.html')
         self.assertTrue(page.context['products_active'])
         self.assertQuerysetEqual(page.context['products'],
-                                 Product.objects.all().order_by('-date_added'))
+                                 Product.objects.all().order_by('-date_added'),
+                                 transform=lambda x: x)
 
     def test_render_product_detail(self):
-        valid_product.save()
         page = self.client.get(f'/product/{valid_product.id}')
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed('product_detail.html')
@@ -26,7 +31,6 @@ class TestViews(TestCase):
         self.assertTrue(page.context['categories_active'])
 
     def test_render_404_not_found(self):
-        valid_product.save()
         page = self.client.get('/product/0')
         self.assertEqual(page.status_code, 404)
         self.assertTemplateUsed('404.html')
