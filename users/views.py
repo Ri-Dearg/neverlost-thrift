54f464056e729f19e57ce1ced231f4e6d0d686d7
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-12 22:55:36 +0000

    Created account redirect so user can only access their own profile page

--- users/views.py
+++ users/views.py
@@ -5,7 +5,18 @@ from django.views.generic import DetailView
 from django.contrib.auth.models import User
 
 
+@login_required
+def access_only_own_profile(request):
+    user_id = request.user.id
+    reverse('users:detail', user_id)
+
+
 class UserProfileDetailView(LoginRequiredMixin, DetailView):
     model = User
     context_object_name = 'user'
     template_name = 'users/user_detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = User.objects.get(pk=self.request.user.id)
+        return context
