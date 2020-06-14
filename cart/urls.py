8f120c70f252fc0495009330cc9224ab7d2326d0
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-14 01:56:04 +0000

    Added remove from cart view

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import add_to_cart
+from .views import add_to_cart, remove_from_cart
 
 urlpatterns = [
     path('add/<int:item_id>/',
-         add_to_cart, name='add_to_cart')
+         add_to_cart, name='add_to_cart'),
+    path('remove/<int:item_id>/',
+         remove_from_cart, name='remove_from_cart')
 ]
--- cart/views.py
+++ cart/views.py
@@ -7,18 +7,44 @@ def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
     next = request.GET.get('next', '')
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        # cart[item_id] += quantity
-        # messages.success(request, f'Updated {product.name} quantity to \
-        #     {cart[item_id]}')
-        pass
-    else:
-        cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your cart')
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        quantity = int(request.POST.get('quantity'))
+        cart = request.session.get('cart', {})
 
-    request.session['cart'] = cart
-    return HttpResponseRedirect(next)
+        if item_id in list(cart.keys()):
+            # cart[item_id] += quantity
+            # messages.success(request, f'Updated {product.name} quantity to \
+            #     {cart[item_id]}')
+            pass
+        else:
+            cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
+
+        request.session['cart'] = cart
+        return HttpResponseRedirect(next)
+    except Exception as e:
+        messages.error(request, f'Error adding item: {e}')
+        return HttpResponseRedirect(next)
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    next = request.GET.get('next', '')
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        cart = request.session.get('cart', {})
+        print(item_id)
+
+        cart.pop(str(item_id))
+        messages.success(request, f'Removed {product.name} from your cart')
+
+        request.session['cart'] = cart
+        return HttpResponseRedirect(next)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponseRedirect(next)
