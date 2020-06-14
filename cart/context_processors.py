800d7d9ee1cc193b15cd4ecfc5155f235c2cfec8
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-14 16:43:07 +0000

    Updated cart context processor and templates with more information.

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,3 +1,26 @@
+from django.shortcuts import get_object_or_404
+
+from products.models import Product
+
+
 def get_cart(request):
     cart = request.session.get('cart')
-    return {'cart': cart}
+    cart_items = []
+    cart_total = 0
+    cart_quantity = 0
+
+    if cart:
+        cart_quantity += len(cart)
+        for item_id, item_data in cart.items():
+            product = get_object_or_404(Product, pk=item_id)
+            cart_total += item_data * product.price
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+
+    return {'cart': cart,
+            'cart_quantity': cart_quantity,
+            'cart_items': cart_items,
+            'cart_total': cart_total}
