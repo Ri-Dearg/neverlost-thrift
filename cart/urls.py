3ba995ffe734e711c37d2b2e55f9746b9ca847b1
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-14 23:12:51 +0000

    Reformatted urls

--- cart/urls.py
+++ cart/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from .views import add_to_cart, remove_from_cart, CartListView
 
 urlpatterns = [
-    path('',
-         CartListView.as_view(), name='list'),
-    path('add/<int:item_id>/',
-         add_to_cart, name='add_to_cart'),
-    path('remove/<int:item_id>/',
-         remove_from_cart, name='remove_from_cart')
+    path('', CartListView.as_view(), name='list'),
+    path('add/<int:item_id>/', add_to_cart, name='add_to_cart'),
+    path('remove/<int:item_id>/', remove_from_cart, name='remove_from_cart')
 ]
