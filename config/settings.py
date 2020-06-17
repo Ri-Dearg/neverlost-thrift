24deb213cbae583ed1a17509660111a8750ae0fe
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-17 15:04:37 +0000

    Adjusted settings for testing collectstatic bug in deployment.

--- config/settings.py
+++ config/settings.py
@@ -229,7 +229,7 @@ USE_TZ = True
 
 # Static files are maintained in a base directory
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
