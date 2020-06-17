e7884649a4f6764c26080ab85dfcf0a8d8e70720
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-17 17:30:04 +0000

    Revert "Removed whitenoise for attempted collectstatic bugfix"
    This reverts commit 6e640915b86019808948fbebd819a470b550aede.
    
    Undo changes made for bugtesting with deployment.

--- config/settings.py
+++ config/settings.py
@@ -105,8 +105,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # # Deploys static files in production
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    # Deploys static files in production
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
