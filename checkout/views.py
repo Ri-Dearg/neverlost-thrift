cbbc64102b0d86906f6657e966ef2cd099e9e288
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-19 18:03:17 +0000

    PaymentIntent and client secret for Stripe added

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,8 @@ from products.models import Product
 from .models import Order
 from cart.context_processors import get_cart
 
+import stripe
+
 
 class OrderCreateView(CreateView):
     model = Order
@@ -24,20 +26,29 @@ class OrderCreateView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
+
         cart_contents = get_cart(self.request)
         total = cart_contents['cart_total']
+
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
         cart_dict = self.request.session.get('cart', {})
         cart_list = []
         for key in cart_dict:
             cart_list.append(key)
         products = Product.objects.filter(id__in=cart_list)
-        stripe_public_key = settings.STRIPE_PUBLIC_KEY
-        stripe_secret_key = settings.STRIPE_SECRET_KEY
 
         order_form = context['form']
 
         context['stripe_public_key'] = stripe_public_key
-        context['stripe_secret_key'] = stripe_secret_key
+        context['client_secret'] = intent.client_secret
         context['products'] = products
         context['order_form'] = order_form
         return context
