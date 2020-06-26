ca6d4634b51d29ee3be7fafc19005173fab07df8
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-26 12:51:01 +0000

    Stockdrop, categories added to context processors, updated appropriately

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,8 @@ TEMPLATES = [
                 # Processor for the cart
                 'cart.context_processors.get_cart',
                 'likes.context_processors.get_likes',
+                'products.context_processors.get_stockdrops',
+                'products.context_processors.get_categories',
             ],
         },
     },
---
+++ products/context_processors.py
@@ -0,0 +1,13 @@
+from .models import StockDrop, Category
+
+
+def get_stockdrops(request):
+    """Provides stockdrop info for the header on all pages"""
+    stockdrops = StockDrop.objects.filter(display=True)
+    return {'stockdrops': stockdrops}
+
+
+def get_categories(request):
+    """Provides category info for the header on all pages"""
+    categories = Category.objects.all().order_by('name')
+    return {'categories': categories}
