85da99fe65f3e28cffd9deb505944a6814b524af
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-26 12:52:53 +0000

    Created categories view, updated header

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView, StockDropDetailView
+from .views import (CategoryDetailView,
+                    ProductListView,
+                    ProductDetailView,
+                    StockDropDetailView)
 
 urlpatterns = [
     path('', ProductListView.as_view(), name="product-list"),
+    path('category/<int:pk>/',
+         CategoryDetailView.as_view(),
+         name="category-detail"),
     path('product/<int:pk>/',
          ProductDetailView.as_view(), name='product-detail'),
     path('stockdrop/<int:pk>/',
