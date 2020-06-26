a997292b999cc31f0a7b840c3fb0196097a4f278
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-26 02:25:46 +0000

    Added Static_ROOT setting

--- config/settings.py
+++ config/settings.py
@@ -233,6 +233,7 @@ USE_TZ = True
 # Static files are maintained in a base directory
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
