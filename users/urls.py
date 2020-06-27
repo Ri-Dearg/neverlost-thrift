865fb8c9b550620faac6f8cdb4b8ec4acb13126f
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-27 21:12:55 +0000

    Password change implemented

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from .views import UserProfileDetailView, CustomEmailView
+from .views import (UserProfileDetailView,
+                    CustomEmailView,
+                    CustomPasswordChangeView)
 
 urlpatterns = [
     path('profile/<int:pk>/', login_required(UserProfileDetailView.as_view()),
          name='user-detail'),
     path('accounts/email/', login_required(CustomEmailView.as_view()),
          name='user-email'),
+    path('accounts/password/change/',
+         login_required(CustomPasswordChangeView.as_view()),
+         name='user-change-password'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,7 +3,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
 from django.contrib.auth.models import User
 
-from allauth.account.views import EmailView, AddEmailForm
+from allauth.account.views import (EmailView,
+                                   AddEmailForm,
+                                   ChangePasswordForm,
+                                   PasswordChangeView,
+                                   sensitive_post_parameters_m)
 from allauth.account.utils import sync_user_email_addresses
 
 
@@ -24,16 +28,18 @@ class UserProfileDetailView(LoginRequiredMixin, DetailView):
         field_names, values = profile._readable_field()
 
         add_email_form = AddEmailForm
+        change_password_form = ChangePasswordForm
 
         context['field_names'] = field_names
         context['values'] = values
         context['user'] = user
         context['profile'] = profile
         context['add_email_form'] = add_email_form
+        context['change_password_form'] = change_password_form
         return context
 
 
-class CustomEmailView(EmailView):
+class CustomEmailView(LoginRequiredMixin, EmailView):
     """Custom email editing view to maintain use on the profile page"""
 
     template_name = 'users/user_detail.html'
@@ -44,4 +50,62 @@ class CustomEmailView(EmailView):
 
         self.success_url = reverse_lazy('users:user-detail',
                                         kwargs={'pk': user_id})
-        return super(EmailView, self).dispatch(request, *args, **kwargs)
+        return super(CustomEmailView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        """Adds all necessary information to the context"""
+        context = super().get_context_data(**kwargs)
+        # Selects the request user as user id no matter id is put in the url
+        user = User.objects.get(pk=self.request.user.id)
+        profile = user.userprofile
+
+        # Makes field names user friendly
+        field_names, values = profile._readable_field()
+
+        add_email_form = AddEmailForm
+        change_password_form = ChangePasswordForm
+
+        context['field_names'] = field_names
+        context['values'] = values
+        context['user'] = user
+        context['profile'] = profile
+        context['add_email_form'] = add_email_form
+        context['change_password_form'] = change_password_form
+        return context
+
+
+class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    """Custom password change view to maintain use on the profile page"""
+
+    template_name = 'users/user_detail.html'
+
+    @sensitive_post_parameters_m
+    def dispatch(self, request, *args, **kwargs):
+        user_id = User.objects.get(pk=self.request.user.id).id
+
+        self.success_url = reverse_lazy('users:user-detail',
+                                        kwargs={'pk': user_id})
+
+        return super(CustomPasswordChangeView, self).dispatch(
+            request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        """Adds all necessary information to the context"""
+        context = super().get_context_data(**kwargs)
+        # Selects the request user as user id no matter id is put in the url
+        user = User.objects.get(pk=self.request.user.id)
+        profile = user.userprofile
+
+        # Makes field names user friendly
+        field_names, values = profile._readable_field()
+
+        add_email_form = AddEmailForm
+        change_password_form = ChangePasswordForm
+
+        context['field_names'] = field_names
+        context['values'] = values
+        context['user'] = user
+        context['profile'] = profile
+        context['add_email_form'] = add_email_form
+        context['change_password_form'] = change_password_form
+        return context
