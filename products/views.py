9595e60b0717c9742f966a2d434561552f007e87
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-27 02:28:25 +0000

    Popovers added to header icons. Templates added.

--- products/views.py
+++ products/views.py
@@ -36,6 +36,16 @@ class ProductListView(ListView):
 
         return Product.objects.all()
 
+    def get_context_data(self, **kwargs):
+        """Adds all necessary information to the context"""
+        context = super().get_context_data(**kwargs)
+        # Selects the active tab
+        if 'query' not in self.request.GET and self.request.path == '/':
+            all_products_active = True
+            context['all_products_active'] = all_products_active
+
+        return context
+
 
 class ProductDetailView(DetailView):
     """Renders the product detail page"""
