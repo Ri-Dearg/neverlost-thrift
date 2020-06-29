9e9c6a19781656e22a797ffff0999a43b46322c1
Ri-Dearg <sheridan.rp@gmail.com>
2020-06-29 02:21:57 +0000

    Likes tests updated for ajax

--- likes/tests/test_views.py
+++ likes/tests/test_views.py
@@ -19,52 +19,54 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'likes/likes_list.html')
 
     def test_confirm_add_to_likes(self):
-        response = self.client.post('/likes/add/1/?next=/product/1/')
+        self.client.post('/likes/ajax/toggle/', {'item-id': 1},
+                         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         session = self.client.session
 
-        self.assertRedirects(response, '/product/1/')
-        self.assertEqual(session['likes'], [1])
+        self.assertEqual(session['likes'], ['1'])
 
         test_user = User.objects.latest('date_joined')
         self.client.force_login(test_user)
-        response = self.client.post('/likes/add/2/?next=/product/1/')
+        self.client.post('/likes/ajax/toggle/', {'item-id': 2},
+                         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertTrue(len(test_user.userprofile.liked_products.all()) == 2)
 
     def test_error_on_incorrect_item_added(self):
-        response = self.client.post('/likes/add/0/?next=/product/1/',
-                                    )
+        self.client.post('/likes/ajax/toggle/', {'item-id': 0},
+                         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
 
-        self.assertRedirects(response, '/product/1/')
         self.assertRaises(Exception, msg='Error adding item: 0')
 
     def test_successfully_remove_from_likes(self):
-        self.client.post('/likes/add/1/?next=/product/1/', )
+        self.client.post('/likes/ajax/toggle/', {'item-id': 1},
+                         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         session = self.client.session
 
-        self.assertEqual(session['likes'], [1])
+        self.assertEqual(session['likes'], ['1'])
 
-        response = self.client.post('/likes/remove/1/?next=/product/1/')
+        self.client.post('/likes/ajax/toggle/', {'item-id': 1},
+                         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         session = self.client.session
 
         self.assertEqual(session['likes'], [])
-        self.assertRedirects(response, '/product/1/')
 
         test_user = User.objects.latest('date_joined')
         self.client.force_login(test_user)
-        self.client.post('/likes/add/2/?next=/product/1/')
-        self.client.post('/likes/remove/2/?next=/product/1/')
+        self.client.post('/likes/ajax/toggle/', {'item-id': 2},
+                         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.client.post('/likes/ajax/toggle/', {'item-id': 2},
+                         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertTrue(len(test_user.userprofile.liked_products.all()) == 0)
 
     def test_error_on_incorrect_item_removed(self):
-        response = self.client.post('/likes/add/1/?next=/product/1/',
-                                    )
+        self.client.post('/likes/ajax/toggle/', {'item-id': 1},
+                         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         session = self.client.session
 
-        self.assertEqual(session['likes'], [1])
+        self.assertEqual(session['likes'], ['1'])
 
-        response = self.client.post('/likes/remove/0/?next=/product/1/')
+        self.client.post('/likes/ajax/toggle/', {'item-id': 0})
 
         session = self.client.session
 
-        self.assertRedirects(response, '/product/1/')
         self.assertRaises(Exception, msg='Error removing item: 0')
