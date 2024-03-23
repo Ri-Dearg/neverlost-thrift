6b0bd8bfa7d3d50eabc1c024c03fdf9d7dc7e48c
Ri-Dearg <sheridan.rp@gmail.com>
2024-03-23 19:14:19 +0100

    feat: add setting dor hosting on render

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,11 @@ elif DEBUG is False:
 
 ALLOWED_HOSTS = ['neverlost-thrift.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
