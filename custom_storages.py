90b4ae62ec45b7ec360303e50f94edd8655bde69
Ri-Dearg <sheridan.rp@gmail.com>
2024-03-24 21:13:52 +0100

    doc: added docstrings to storage

--- custom_storages.py
+++ custom_storages.py
@@ -1,22 +1,27 @@
+"""Storage class for supabase storage bucket."""
 from django.core.files.storage import Storage
 from supabase import create_client
+
 from config import settings
 
 
+# Code taken from
+# https://github.com/jschneier/django-storages/issues/1099#issuecomment-1975178634
 class SupabaseStorage(Storage):
+    """Connect to Supabase Storage for saving files."""
+
     def __init__(self, bucket_name=settings.BUCKET_NAME, **kwargs):
+        """Create connection and settings."""
         self.bucket_name = bucket_name
 
         self.supabase = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
-        print(self.bucket_name)
         self.storage_client = self.supabase.storage.from_(self.bucket_name)
 
     def _open(self, name, mode="rb"):
-        # Implement the method to open a file from Supabase
-        pass
+        """Implement the method to open a file from Supabase."""
 
     def _save(self, name, content):
-        # Implement the method to save a file to Supabase
+        """Implement the method to save a file to Supabase."""
         content_file = content.file
         content_file.seek(0)  # Move the file pointer to the beginning
         content_bytes = content_file.read()
@@ -26,9 +31,8 @@ class SupabaseStorage(Storage):
         return data.json()["Key"]  # name/path of the file
 
     def exists(self, name):
-        # Implement the method to check if a file exists in Supabase
-        pass
+        """Implement the method to check if a file exists in Supabase."""
 
     def url(self, name):
-        # Implement the method to return the URL for a file in Supabase
+        """Implement the method to return the URL for a file in Supabase."""
         return f"{settings.SUPABASE_URL}/storage/v1/object/public/{name}"
