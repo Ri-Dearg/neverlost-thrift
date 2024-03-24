7d130ae66061b37daeb578ca2b2a02826344ab5c
Ri-Dearg <sheridan.rp@gmail.com>
2024-03-25 00:29:00 +0100

    fix: removal of .scss files from compilation

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py compilescss --traceback
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --ignore=*.scss
 python manage.py makemigrations && python manage.py migrate
